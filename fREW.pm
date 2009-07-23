cc394784cea53301134858c5603e76c97fad63b3
Frew Schmidt <frioux@gmail.com>
2009-07-23 09:56:25 -0500

    init

---
+++ fREW.pm
@@ -0,0 +1,32 @@
+package fREW;
+use strict;
+use warnings;
+use feature ':5.10';
+my $fiSMBoC = [
+   q{%s is Station's Most Bodacious Creation}
+];
+my $RESEARCH = [ 'Robots Eagerly Sailing Epic Artificial Rhythmic Cyclical Homonyms' ];
+my $IMAGINATIVE = [ 'Insane Mimicries of Amazingly Gorgeous, Incomplete Networks, Axiomatic Theorems, and Immortally Vivacious Ecstasy' ];
+my $ORGANIC = [ 'Original Renditions of Genetic Art Naturally Increasing in Complexity' ];
+my $UNIFICATIONS = [ 'Unions Normally Identified From Initial Characters; Aesthetically Tailored to Infer Other Notions Subconsciously' ];
+my $fRIOUX = [
+   q{%s %ses %s %s %s like XUOIRf},
+   $fiSMBoC,
+   $RESEARCH,
+   $IMAGINATIVE,
+   $ORGANIC,
+   $UNIFICATIONS,
+];
+my $fRUE = [
+   q{%s's Rectitude is Underpinned by Equivalence},
+   $fRIOUX,
+];
+my $fROOH = [
+   q{%s: Robotic Ominous Ossified Herald},
+   $fRUE,
+];
+my $fREW = [
+   q{%s Represents Encephelon Welkin},
+   $fROOH,
+];
+push @{$fiSMBoC}, $fREW;
